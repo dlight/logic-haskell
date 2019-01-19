c9766a1bf303761d394f072c4ac40863a307b622
Vitor Greati <greati@ufrn.edu.br>
2019-01-19 14:19:32 -0300

    Fix S_i and implement P_4

--- Axiomatization.hs
+++ Axiomatization.hs
@@ -107,7 +107,6 @@ getDefaultConnective :: [(String, TruthTable Int)]
 getDefaultConnective = connectives
 
 -- S2 Axiomatization - 2(v)
-
 s2_1 = read "{P} | +(P, Q)" :: Consequence
 s2_2 = read "{+(P, P)} | P" :: Consequence
 s2_3 = read "{+(P, Q)} | +(Q, P)" :: Consequence
@@ -116,19 +115,29 @@ s2Axiomatization = [s2_1, s2_2, s2_3, s2_4]
 s2Signature = fromRight M.empty $ sigmaFromConseqRelation (S.fromList s2Axiomatization)
 
 -- S4 Axiomatization - 2(v, 1)
-s4_0 = read "{} | /()" :: Consequence
+s4_0 = read "{} | .()" :: Consequence
 s4Axiomatization = s4_0 : s2Axiomatization
 s4Signature = fromRight M.empty $ sigmaFromConseqRelation (S.fromList s4Axiomatization)
 
 -- S5 Axiomatization - 2(v, 0)
-s5_0 = read "{+(P, .())} | P" :: Consequence
+s5_0 = read "{+(P, /())} | P" :: Consequence
 s5Axiomatization = s5_0 : s2Axiomatization
 s5Signature = fromRight M.empty $ sigmaFromConseqRelation (S.fromList s5Axiomatization)
 
 -- S6 Axiomatization - 2(v, 0, 1)
-s6_0 = read "{} | /()" :: Consequence
+s6_0 = read "{} | .()" :: Consequence
 s6Axiomatization = s6_0 : s5Axiomatization
 s6Signature = fromRight M.empty $ sigmaFromConseqRelation (S.fromList s6Axiomatization)
+
+-- P2 Axiomatization - 2(^)
+p2_1 = read "{P, Q} | *(P, Q)" :: Consequence
+p2_2 = read "{*(P, Q)} | P" :: Consequence
+p2_3 = read "{*(P, Q)} | Q" :: Consequence
+p2Axiomatization = [p2_1, p2_2, p2_3]
+
+-- P4 Axiomatization - 2(^, 0)
+-- P5 Axiomatization - 2(^, 1)
+-- P6 Axiomatization - 2(^, 0, 1)
 
 -- A4 Axiomatization - 2(v,^)
 
