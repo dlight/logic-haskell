279c562eb5b9c1b091d55036569f37158b7da692
Vitor Greati <greati@ufrn.edu.br>
2019-05-01 11:33:38 -0300

    Implement making derived connectives from projections

--- Formula.hs
+++ Formula.hs
@@ -7,6 +7,7 @@ import Signature
 import qualified Data.Map.Strict as M
 import qualified Data.Set as S
 import qualified Data.List as L
+import qualified Data.Maybe as DMaybe
 import Control.Exception.Safe (Exception, MonadThrow, throwM, SomeException)
 import Data.Typeable
 import Data.Aeson.Types (ToJSON)
@@ -23,6 +24,8 @@ data Formula =
     Var String | 
     Op Symbol [Formula]
     deriving (Show, Eq, Ord, Generic, ToJSON)
+
+type Substitution = M.Map Formula Formula 
 
 -- | Parser for formulas
 instance Read Formula where
@@ -75,6 +78,13 @@ sigmaVarsFromFormula (Op op fs) =
                                         (sigma, vars') <- msigma
                                         return (M.unionWith S.union sigma sff, S.union vars vars')
 
+-- | Compute the arity of a formula
+getFormulaArity :: (MonadThrow m) => Formula -> m Int
+getFormulaArity fmla = 
+    case sigmaVarsFromFormula fmla of
+        Left e -> throwM e
+        Right (_, vars) -> return $ S.size vars
+
 substitution :: Variable -> Formula -> Formula -> Formula
 substitution v f f'@(Var s) = if v == s then f else f'
 substituiion v f (Op s fs) = Op s (map (substitution v f) fs)
@@ -85,15 +95,53 @@ translation t (Op s' fs) = flip t <$> mkConnective (s',(length fs)) <*> mapM (tr
 
 -- | Given an arity and a list of variables, produce tuples of these variables
 -- to be applied in a connective of that arity
-makeVariablesPermsUpToIso :: Int -> S.Set [String]
+makeVariablesPermsUpToIso :: Int -> S.Set [Formula]
 makeVariablesPermsUpToIso originalArity = 
         S.unions $ makeNonRepeatedPermutations <$> [1..originalArity]
     where
-        makeNonRepeatedPermutations :: Int -> S.Set [String]
         makeNonRepeatedPermutations = S.fromList . L.permutations . makeVarSet originalArity
-        makeVarSet :: Int -> Int -> [String]
         makeVarSet originalArity currentArity = 
-            replicate nrep1 "p1" ++ map (("p"++).show) [2..currentArity]
+            replicate nrep1 (Var "p1") ++ map (Var . ("p"++) . show) [2..currentArity]
                 where 
                     nrep1 = originalArity - currentArity + 1
                     unrep = originalArity - nrep1    
+
+-- | Produce substitutions from a Formula
+-- 1. extract the variable set of a formula, and infer its arity from it
+-- 2. produce substitutions given the variables and a set of formulas
+-- example: makeSubstitutions "+(A,B)" "{["B", "A"],["A","A"]}"
+makeSubstitutions :: (MonadThrow m) => Formula -> S.Set [Formula] -> m [Substitution]
+makeSubstitutions target source = 
+    case sigmaVarsFromFormula target of
+        Left e -> throwM e
+        Right (_, vars) -> return $ foldr ((:) . mkSubsFromSets varList) [] source
+            where
+                varList = S.toList $ S.map Var vars
+                mkSubsFromSets :: [Formula] -> [Formula] -> Substitution
+                mkSubsFromSets varList = M.fromList . zip varList
+
+-- | Apply a substitution in a formula
+applySubstitution :: Substitution -> Formula -> Formula
+applySubstitution subs v@(Var s) = DMaybe.fromMaybe v ((M.!?) subs v)
+applySubstitution subs fmla@(Op symb ops) = 
+    DMaybe.fromMaybe (Op symb (applySubstitutionLeaves subs <$> ops)) ((M.!?) subs fmla)
+
+-- | Apply a substitution only in the leaves of a formula
+applySubstitutionLeaves :: Substitution -> Formula -> Formula
+applySubstitutionLeaves subs v@(Var s) = DMaybe.fromMaybe v ((M.!?) subs v)
+applySubstitutionLeaves subs fmla@(Op symb ops) = Op symb (applySubstitutionLeaves subs <$> ops)
+
+-- | Given a derived connective (a formula), produces all the derived connectives
+-- induced com multiple finitarily composing it with all possible projections
+-- of smaller or equal arity.
+makeDerivedFromProjections :: (MonadThrow m) => Formula -> m [Formula]
+makeDerivedFromProjections fmla = 
+    case getFormulaArity fmla of
+        Left e -> throwM e
+        Right n ->
+            case makeSubstitutions fmla (makeVariablesPermsUpToIso n) of
+                Left e -> throwM e
+                Right subs -> 
+                    return $ flip applySubstitutionLeaves fmla <$> subs
+
+
