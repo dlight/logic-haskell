2ac397c7e98bf77d30dd1004eb265991526d67ed
Vitor Greati <greati@ufrn.edu.br>
2019-05-01 17:06:56 -0300

    Start clone impl

--- Connective.hs
+++ Connective.hs
@@ -10,7 +10,19 @@ import GHC.Generics
 type Arity = Int
 
 data Connective = Connective {
-                    symbol :: String,
-                    k :: Arity
-                    } | DerivedConnective Formula 
+                        symbol :: String,
+                        k :: Arity
+                    } | 
+                    DerivedConnective {
+                        symbol :: String,
+                        fmla :: Formula 
+                    }
                     deriving (Show, Typeable, Eq, Ord, Generic, ToJSON, FromJSON, ToJSONKey)
+
+-- | Induces a formula from a connective
+formulaFromConnective :: Connective -> Formula
+formulaFromConnective (Connective s k) = Op s variables
+    where variables = map (Var . ("p"++) . show) [1..k]
+formulaFromConnective (DerivedConnective _ fmla) = fmla
+
+
